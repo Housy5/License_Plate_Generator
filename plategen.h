d1989492f26c2b571ff54bdb12461300a6dcf168
Housy <50631944+Housy5@users.noreply.github.com>
2023-08-15 17:16:10 +0200

    Delete plategen.h

--- plategen.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLATEGEN_H_INCLUDED
-#define PLATEGEN_H_INCLUDED
-
-void initRandomizer();
-char getRandomNumber();
-char getRandomCharacter();
-char * generatePlate(char * format, char * dest);
-
-#endif // PLATEGEN_H_INCLUDED
